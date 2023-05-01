9d4c53910b5a6540c6f7253a68c2c9f4fd6838d8
Derek Sutton <dereksutton86@gmail.com>
2023-05-01 14:34:24 -0400

    finished testing all routes for Products & Tags in Insomnia, fixed minor bugs - all checks passing

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -65,15 +65,17 @@ router.post('/', async (req, res) => {
 
 //update product
 router.put('/:id', async (req, res) => {
-    try {
-      await Product.update(req.body, {
-        where: {
-          id: req.params.id,
-        },
-      });
-  
-      const productTags = await ProductTag.findAll({ where: { product_id: req.params.id } });
-      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+  try {
+    await Product.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    const productTags = await ProductTag.findAll({ where: { product_id: req.params.id } });
+    const productTagIds = productTags.map(({ tag_id }) => tag_id);
+
+    if (req.body.tagIds) {
       const newProductTags = req.body.tagIds
         .filter((tag_id) => !productTagIds.includes(tag_id))
         .map((tag_id) => {
@@ -82,21 +84,22 @@ router.put('/:id', async (req, res) => {
             tag_id,
           };
         });
-  
+
       const productTagsToRemove = productTags
         .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
         .map(({ id }) => id);
-  
+
       await Promise.all([
         ProductTag.destroy({ where: { id: productTagsToRemove } }),
         ProductTag.bulkCreate(newProductTags),
       ]);
-  
-      res.status(200).json({ message: 'Product successfully updated.' });
-    } catch (err) {
-      res.status(400).json(err);
     }
-  });  
+
+    res.status(200).json({ message: 'Product successfully updated.' });
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
